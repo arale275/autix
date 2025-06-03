3d34a88e97a84eae5ace2614deff62f3735cf0a8
arale walles <arale275@gmail.com>
2025-06-03 03:02:41 +0300

    Changes to be committed: 	deleted:    client/src/app/favicon.ico 	modified:   client/src/app/layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -84,12 +84,6 @@ export default function RootLayout({
           crossOrigin=""
         />
 
-        {/* Favicon and app icons */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-
         {/* Theme color for mobile browsers */}
         <meta name="theme-color" content="#3b82f6" />
         <meta name="color-scheme" content="light" />
